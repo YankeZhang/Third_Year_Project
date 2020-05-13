d744c74d780bb25ac3f615671c410cd09b5ce860
YankeZhang <zczly02@ucl.ac.uk>
2020-05-13 13:37:14 +0800

    change database

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cors(corsOptions))
 //database configuration
 const connection = mysql.createConnection({
     host     : 'localhost',
+    port     :   3306,
     user     : 'root',
     password : '123',
     database : 'IBMProject'
